4a87b59f826c3adc5e85caf33306c7602da04eb5
ZohreScript <z.omidi74@gmail.com>
2024-08-17 11:40:06 +0330

    gajino

--- src/utils/toPersianNumbers.ts
+++ src/utils/toPersianNumbers.ts
@@ -1,27 +1,20 @@
 const farsiDigits: string[] = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
 
 export function toPersianNumbersWithComma(n: number): string {
-  // ابتدا عدد را به رشته‌ای با جداکننده‌های کاما تبدیل می‌کنیم
   const numWithCommas: string = numberWithCommas(n);
   
-  // سپس اعداد انگلیسی در این رشته را به اعداد فارسی تبدیل می‌کنیم
   return toPersianNumbers(numWithCommas);
 }
 
 function numberWithCommas(x: number): string {
-  // عدد را به رشته‌ای تبدیل می‌کنیم و هر سه رقم را با کاما جدا می‌کنیم
-  return x.toLocaleString(); // استفاده از toLocaleString برای جدا کردن هزارگان
-}
+  return x.toLocaleString(); }
 
 export function toPersianNumbers(n: string | number): string {
-  // اعداد انگلیسی را به اعداد فارسی تبدیل می‌کنیم
   return n.toString().replace(/\d/g, (x) => farsiDigits[parseInt(x)]);
 }
 
 export function toPersianNumbersInText(text: string): string {
-    // یافتن تمامی اعداد در متن و تبدیل آن‌ها به اعداد فارسی همراه با کاما
     return text.replace(/\d+/g, (match) => {
-        // عدد را به فارسی با کاما تبدیل می‌کنیم
         return toPersianNumbersWithComma(Number(match));
     });
 }
